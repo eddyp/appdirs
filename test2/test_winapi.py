7481f8c8339e757ed23f0e563fa43b1c29304124
Eddy Petrișor <eddy.petrisor@gmail.com>
2013-03-23 00:06:35 +0200

    monkeypatch example for sys.platform

---
+++ test2/test_winapi.py
@@ -0,0 +1,49 @@
+import pytest
+if __name__ != "__main__":
+    import sys
+    sys.path.append('.')
+    import appdirs
+
+class _winregmock:
+    def OpenKey(key, subkey, res=None, sam=None):
+        pass
+    def QueryValueEx(key,  valuename):
+        pass
+
+mocks = {'_winreg':_winregmock}
+
+def helper_patchpair(monkey, modname, apiname, retval):
+    def mockreturn(a=None, b=None, c=None, d=None, e=None):
+        return retval
+    monkey.setattr(modname, apiname, mockreturn)
+
+#@pytest.mark.parametrize(
+        #("platform", "patchpairs", "mockpairs", "roaming", "envvar"), 
+            #(   "win32",
+                #{ '_winreg':
+                    #{ 'OpenKey': 'fake\\_winreg\\key',
+                      #'QueryValueEx': 'c:\\fake\\shell\\folder'
+                    #},
+                  #'win32com':None, 
+                  #'ctypes':None
+                #}, 
+                #False,
+                #{}
+            #)
+        #)
+@pytest.mark.parametrize(
+            ("mod", "attr", "ret"),
+            [ ("sys", "platform", "win32"),
+              ("sys", "platform", "Linux2"),
+              ("sys", "platform", "darwin")
+            ]
+        )
+def test_config_dir(monkeypatch, mod, attr, ret):
+    import sys
+    modobj = sys.modules[mod]
+    helper_patchpair(monkeypatch, modobj, attr, ret)
+    assert sys.platform() == ret
+
+if __name__=="__main__":
+    pytest.main([ __file__ ])
+    import appdirs
